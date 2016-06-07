d9b0b014b57a398550d4bd0ba587908f021533c9
Nuria <ngarciacarrillo@danielcastelao.com>
2016-06-07 13:19:43 +0200

    se definen variables

---
+++ src/AccesoBD.java
@@ -0,0 +1,23 @@
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Nuria
+ */
+ class AccesoBD {
+/**
+ * @param bd Esta variable definira cual es el nombre de nuestra base de datos
+ * @param url Está variable definira cual es la URL en la que se encuentra nuestra bd
+ * @param user Está variable definira cual es el usuario para acceder a la BD
+ * @param pass Está variable definira cual es el usuario
+ * @param link Es de tipo Connection y sera el parametro mediante el cual se definira 
+ * la conexión con la BD
+ */
+    public String db = null;
+    public String url =null;
+    public String user = null;
+    public String pass =null;
+    Connection link = null;
+ 
+}
