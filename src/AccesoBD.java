44c321e03e2e6adfec2a6d57194ba02283ba704d
Nuria <ngarciacarrillo@danielcastelao.com>
2016-06-07 13:37:26 +0200

    variables definidas y metodo conectar, se añade metodo añadir

--- src/AccesoBD.java
+++ src/AccesoBD.java
@@ -1,6 +1,7 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -9,33 +10,58 @@ import java.util.logging.Logger;
  *
  * @author Nuria
  */
- class AccesoBD {
-/**
- * @param bd Esta variable definira cual es el nombre de nuestra base de datos
- * @param url Está variable definira cual es la URL en la que se encuentra nuestra bd
- * @param user Está variable definira cual es el usuario para acceder a la BD
- * @param pass Está variable definira cual es el usuario
- * @param link Es de tipo Connection y sera el parametro mediante el cual se definira 
- * la conexión con la BD
- */
+class AccesoBD {
+
+    /**
+     * @param bd Esta variable definira cual es el nombre de nuestra base de
+     * datos
+     * @param url Está variable definira cual es la URL en la que se encuentra
+     * nuestra bd
+     * @param user Está variable definira cual es el usuario para acceder a la
+     * BD
+     * @param pass Está variable definira cual es el usuario
+     * @param link Es de tipo Connection y sera el parametro mediante el cual se
+     * definira la conexión con la BD
+     */
     public String db = null;
-    public String url =null;
+    public String url = null;
     public String user = null;
-    public String pass =null;
+    public String pass = null;
     Connection link = null;
-    
+
     /**
      * Este metodo establece la conexión con la BD
-     * @return link 
+     *
+     * @return link
      */
-    public Connection conectar(){
-        
+    public Connection conectar() {
+
         try {
-            link = DriverManager.getConnection(url,user,pass);
+            link = DriverManager.getConnection(url, user, pass);
         } catch (SQLException ex) {
             Logger.getLogger(AccesoBD.class.getName()).log(Level.SEVERE, null, ex);
         }
-      
-   return link;
-   }
+
+        return link;
+    }
+   /**
+    * Metodo para añadir un nuevo registro en la tabla de la base de datos
+    * @param tabla nombre de la tabla en la que queremos insertar el registro
+    * @param columnas nombre de las columnas que tiene la tabla
+    * @param datos son los datos que queremos añadir en las columnas de nuestra tabla
+    */
+    
+    public void Añadir(String tabla, String columnas, String datos) {
+
+       
+        try {
+            PreparedStatement stmt = link.prepareStatement("INSERT INTO "+tabla+" ("+columnas+") VALUES("+datos+")");
+            stmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(AccesoBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+
+    }
+
 }
